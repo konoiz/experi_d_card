13c63272059e67f518bd92b90456c90e0526ff95
konoiz <konoiz@yashima.hitobashira.jp>
2015-01-07 17:27:01 +0900

    enable static asset

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,7 +3,6 @@ class Contact
   include ActiveModel::Associations
   
   attr_accessor :name, :department, :subject, :body, :email
-  validates :name,  presence: true
   validates :body, presence: true
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get "cards/update_courses", as: 'update_courses'
   get "cards/update_laboratories", as: 'update_laboratories'
   resources :cards
+  resources :contacts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
